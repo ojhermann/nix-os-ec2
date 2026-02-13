4879358fb75a4fc370856d6f103fd1bf0e46842c
System administrator <root@ip-10-0-13-37.ec2.internal>
2026-02-13 18:04:29 +0000

    create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,45 @@
+{ config, modulesPath, pkgs, ... }: {
+  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+  ec2.efi = true;
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  }; # security.sudo
+  
+  system.stateVersion = "25.11";
+
+  environment.shells = with pkgs; [ zsh ]; # environment.shells
+  environment.systemPackages = with pkgs; [
+    git
+    helix
+    tree
+    zsh
+  ]; # environment.systemPackages
+  environment.variables = {
+    EDITOR = "hx";
+    VISUAL = "hx";
+  }; # environment.variables
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      grep="grep -i --color=auto";
+      ls="ls --color=auto";
+      tree="tree -C";
+    }; # shellAliases
+  }; # programs.zsh
+  
+  users.defaultUserShell = pkgs.zsh;
+  users.users.otto = {
+    isNormalUser = true;
+    description = "otto";
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP98KVJtOlcRc3WhHQb8MaDPPGgQmnNPjquYIa2q+ygH otto-admin-ec2\n"
+    ]; # openssh.authorizedKeys.keys
+  }; # users.users.otto
+}
