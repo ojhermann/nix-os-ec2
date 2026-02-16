8bb2c7c98cae9afc68a28b43e9ec9d20e9d66e84
Otto Hermann <ojhermann@gmail.com>
2026-02-16 17:19:12 +0000

    create home/scripts/ and add some scripts

---
+++ home/scripts/watch-dir-rust.sh
@@ -0,0 +1,12 @@
+CURRENT=$(tree)
+PRIOR="$CURRENT"
+clear
+tree -C -I "target"
+
+while true; do
+	CURRENT=$(tree -C)
+	if [[ "$CURRENT" != "$PRIOR" ]]; then
+		PRIOR="$CURRENT"
+		clear && tree -C -I "target"
+	fi
+done
