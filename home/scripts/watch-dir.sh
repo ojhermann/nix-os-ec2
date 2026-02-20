e29cfed730f5a85f71adef285d8544eb862dae8d
Otto Hermann <ojhermann@gmail.com>
2026-02-20 18:45:53 +0000

    use gst in watch-dir

--- home/scripts/watch-dir.sh
+++ home/scripts/watch-dir.sh
@@ -1,17 +1,6 @@
 cd "${1:-.}" || exit 1
 
-show() {
-	if git rev-parse --git-dir >/dev/null 2>&1; then
-		git status -sb && tree -aC -I '.git' --gitignore
-	else
-		tree -aC
-	fi
-}
-
-clear
-show
-
 fsnotifywait -r -m -e modify,create,delete,move --exclude '\.git' . | while read -r line; do
 	clear
-	show
+	gst
 done
